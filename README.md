d0466f4e7cf70f874b642afdb7fb467912127662
CodingManoj <mvp2612@hotmail.com>
2023-10-02 22:35:23 -0400

    Git Auto Push

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # terraform-loadbalancers
+
+This is the root module where we are going to call the backend module. Here is where we are going to execute the terraform commands.
+
+All the *.tfvars or the files of tfvars with respect to environments will be supplied here.
---
+++ main.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+  source                    = "./vendor/modules/vpc"
+  VPC_CIDR                  = var.VPC_CIDR
+  ENV                       = var.ENV
+  AZ                        = var.AZ 
+  PUBLIC_SUBNET_CIDR        = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR       = var.PRIVATE_SUBNET_CIDR
+  DEFAULT_VPC_ID            = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR          = var.DEFAULT_VPC_CIDR
+  DEFAILT_VPC_RT            = var.DEFAILT_VPC_RT
+  PRIVATE_HOSTED_ZONE_ID    = var.PRIVATE_HOSTED_ZONE_ID
+  PRIVATE_HOSTED_ZONE_NAME  = var.PRIVATE_HOSTED_ZONE_NAME
+  PUBLIC_HOSTED_ZONE_ID     = var.PUBLIC_HOSTED_ZONE_ID
+  PUBLIC_HOSTED_ZONE_NAME   = var.PUBLIC_HOSTED_ZONE_NAME
+
+ 
+}
+
+# We cannot parametrize anything that's added n the source.
+# To limit that, we can use a tool called as Terrafile. All it does is clonnes the remote code from the specified branch and make it locally available.
