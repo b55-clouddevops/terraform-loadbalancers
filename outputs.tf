a3a03cff6a1c7f610144b8ce41213e3b44ff7fdb
CodingManoj <mvp2612@hotmail.com>
2023-10-06 23:04:56 -0400

    Git Auto Push

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
-# output "VPC_ID" {
-#     value = module.vpc.VPC_ID
-# }
+output "PUBLIC_ALB_ARN" {
+    value = module.alb-public.ALB_ARN
+}
+
+output "PRIVATE_ALB_ARN" {
+    value = module.alb-private.ALB_ARN
+}
+
 
 # output "VPC_CIDR" {
 #     value = module.vpc.VPC_CIDR
